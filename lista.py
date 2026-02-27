b1974c672f2d36d6d9180a8f83ee662ba33e56a1
Gustavo Rodrigues Santos <1051618@senacminas.edu.br>
2026-02-27 11:51:19 -0300

    Novos arquivos

---
+++ lista.py
@@ -0,0 +1,13 @@
+lista_nomes = ["Flamengo", "Fluminense", "Botafogo", "Vasco", "Palmeiras", "Grêmio", "Internacional", "América-MG" , "São Paulo", "Corinthians"]
+lista_nomes.insert(1,"LOUD")
+lista_nomes.insert(2,"Fluxo")
+lista_nomes.insert(3,"INTZ")
+
+
+
+
+
+
+
+
+
---
+++ tupla.py
@@ -0,0 +1,4 @@
+aluno = "Gustavo Rodrigues", 17, "R$ 25.000,00"
+nome, idade, pretensão_salarial = aluno
+
+print(f"O meu nome é {nome}, tenho {idade} anos, e quero uma faixa salarial  de {pretensão_salarial} ")
